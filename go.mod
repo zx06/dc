380fb0d9412cb3b872fe13f5163c2196002cd2d5
x_zhuo <x_zhuo@live.com>
2019-06-19 22:58:59 +0800

    complete...

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module dc
 
 go 1.12
+
+replace (
+	golang.org/x/net v0.0.0-20180218175443-cbe0f9307d01 => github.com/golang/net v0.0.0-20180218175443-cbe0f9307d01
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a => github.com/golang/net v0.0.0-20181114220301-adae6a3d119a
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
+)
+
+replace golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 => github.com/golang/sys v0.0.0-20190222072716-a9d3bda3a223
--- main.go
+++ main.go
@@ -1,35 +1,99 @@
 package main
 
 import (
-	"regexp"
 	"fmt"
-	"io/ioutil"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/fatih/color"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 const (
-	API_URL = "http://dict.youdao.com/search?q="
-	
-)
-var(
-	// 音标
-	R_PS = regexp.MustCompile(`"phonetic">(.*?)</span>`)
+	ApiUrl = "http://dict.youdao.com/search?q="
 )
 
-func handleErr(err error,msg string)  {
-	// TODO
+type Word struct {
+	// 关键词
+	Keyword string
+	// 发音
+	Pronounces []string
+	// 翻译
+	Translations []string
 }
 
-func getContent(word string)  {
-	resp,err:=http.Get(API_URL+word)
-	handleErr(err,"response error")
-	defer resp.Body.Close()
-	body,_:=ioutil.ReadAll(resp.Body)
+func removeEmpty(s string) string {
+	s = strings.ReplaceAll(s, "\n", "")
+	s = strings.ReplaceAll(s, " ", "")
+	s = strings.ReplaceAll(s, "\t", "")
+	return s
+}
 
-	t:=R_PS.FindSubmatch(body)
-	fmt.Println(string(t[1]))
+func (w *Word) Parse(keyword string) {
+	resp, err := http.Get(ApiUrl + keyword)
+	handleErr(err, "response error")
+	//defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	handleErr(err, "goquery error")
+
+	// 解析关键词
+	w.Keyword = doc.Find("#phrsListTab > h2 > span").Text()
+
+	// 解析音标
+	doc.Find("#phrsListTab > h2 > div > span").Each(func(i int, s *goquery.Selection) {
+		w.Pronounces = append(w.Pronounces, removeEmpty(s.Text()))
+	})
+
+	doc.Find("#phrsListTab > div > ul > li").Each(func(i int, s *goquery.Selection) {
+		trans := s.Text()
+		w.Translations = append(w.Translations, trans)
+	})
+
+}
+
+func Display(word Word) {
+
+	hiGreen := color.New(color.FgHiGreen)
+	hiCyan := color.New(color.FgCyan)
+	//white:=color.New(color.FgWhite)
+
+	color.HiMagenta("%s\n", word.Keyword)
+
+	for _, v := range word.Pronounces {
+		_, err := hiCyan.Printf("  %s  ", v)
+		handleErr(err, "print error")
+	}
+	fmt.Println()
+
+	for _, v := range word.Translations {
+		vs := strings.SplitN(v, ".", 2)
+		if len(vs) > 1 {
+			_, err := hiGreen.Printf("  %s.", vs[0])
+			handleErr(err, "print error")
+			color.HiBlue("%s\n", vs[1])
+		}else {
+			color.HiBlue("  %s\n", v)
+		}
+
+	}
+
+}
+
+func handleErr(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s > %s\n", msg, err)
+	}
 }
 
 func main() {
-	getContent("love")
+	var args = os.Args
+	if len(args) < 2 {
+		fmt.Println("请输入要查询的单词.")
+		os.Exit(0)
+	}
+	w := &Word{}
+	w.Parse(args[1])
+	Display(*w)
 }
